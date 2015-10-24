2343d0892ddceda8f78698afa1c794894941bbdf
Edo Kreš <kresko1995@gmail.com>
2015-10-24 19:50:10 +0200

    initial commit

--- app/js/main.js
+++ app/js/main.js
@@ -27,57 +27,7 @@
 			},500)
 		});
 
-	$('html').addClass('js');
-
-	var contactForm = {
-
-		container: $('#contact'),
-
-		config: {
-			effect: 'slideToggle',
-			speed: 500
-		},
-
-		init: function(config) {
-			$.extend(this.config, config);
-
-			$('<button></button>', {
-				text: 'Contact Me'
-			})
-				.insertAfter( '.earth-wallmud:first' )
-				.on( 'click', this.show );
-		},
-
-		show: function() {
-			var cf = contactForm,
-				container = cf.container,
-				config = cf.config;
-
-			if ( container.is(':hidden') ) {
-				contactForm.close.call(container);
-				container[config.effect](config.speed);
-			}
-		},
-
-		close: function() {
-			var $this = $(this), // #contact
-				config = contactForm.config;
-
-			if ( $this.find('span.close').length ) return;
-
-			$('<span class=close>X</span>')
-				.prependTo(this)
-				.on('click', function() {
-				// this = span
-					$this[config.effect](config.speed);
-				})
-		}
-	};
-
-	contactForm.init({
-	// effect: 'fadeToggle',
-		speed: 300
-	});	
+	
 
 	var sliderUL = $('div.slider').css('overflow', 'hidden').children('ul'),
 		imgs = sliderUL.find('img'),
